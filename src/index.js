2e4e92dff53f3b9dbc9580794f33749cb3a260ce
davidegaspar <git@davidegaspar.com>
2017-09-03 12:01:26 +0100

    adds lesson 4

--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import './index.css';
 import red01 from './red01.json';
 import red02 from './red02.json';
 import red03 from './red03.json';
+import red04 from './red04.json';
 
 ReactDOM.render(
   <App
-    lessonList = {[red01, red02, red03]}
+    lessonList = {[red01, red02, red03, red04]}
   />,
   document.getElementById('root')
 );
